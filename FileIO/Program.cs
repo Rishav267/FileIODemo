0e2d3460517f77cc53fe09a71d999b16e36b6d7f
Rishav Kedia <rishav267@gmail.com>
2021-07-17 10:57:16 +0530

    Add project files.

---
+++ FileIO/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ReadRec();
+            // XmlSerial();
+            StreamReader reader = new StreamReader("");
+            XmlSerializer xml = new XmlSerializer(typeof(Product);
+            Product p = new Product();
+            p = (Product)xml.Deserialize(reader);
+
+        }
+
+        private static void XmlSerial()
+        {
+            Product p = new Product();
+            p.ProductID = 111;
+            p.Name = "IPhone X";
+            p.cost = 90000;
+            StreamWriter write = new StreamWriter("", true);
+            XmlSerializer xml = new XmlSerializer(typeof(Product));
+            xml.Serialize(write, p);
+            write.Close();
+        }
+
+        private static void ReadRec()
+        {
+            //NewMethod();
+            StreamReader reader = new StreamReader("jdjd");
+            string itemR = reader.ReadLine();
+            Product p = new Product();
+            string[] item = itemR.Split(',');
+            p.ProductID = int.Parse(item[0]);
+            p.Name = item[1];
+            p.cost = int.Parse(item[2]);
+        }
+
+        private static void WriteRec()
+        {
+            Product p = new Product();
+            p.ProductID = 111;
+            p.Name = "IPhone X";
+            p.cost = 90000;
+            StreamWriter write = new StreamWriter("");
+            string item = $"{p.ProductID},{p.Name},{p.cost}";
+            write.WriteLine(item);
+            
+        }
+    }
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public int cost { get; set; }
+
+    }
+}
---
+++ FileIODemo/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIODemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //collect name from user and store into file
+            StreamReader reader = null;
+            try
+            {
+                reader = new StreamReader("C:\\Users\\risha\\OneDrive\\Documents\\names.txt");
+                string line;
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+                    Console.WriteLine(line);
+                    reader.Close();
+                }
+
+                string allData = reader.ReadToEnd();
+                Console.WriteLine(allData);
+
+                File.Delete("");
+                FileInfo info = new FileInfo("C:\\Users\\risha\\OneDrive\\Documents\\names.txt");
+                Console.WriteLine(info.CreationTime);
+                Console.WriteLine(info.Length);
+                Directory.GetFiles("C....");
+                foreach (var f in Directory.GetFiles("C...."))
+                {
+                    Console.WriteLine(f);
+                }
+
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+            //Write();
+
+        }
+
+        private static void Write()
+        {
+            string name;
+            Console.WriteLine("Enter Name: ");
+            name = Console.ReadLine();
+            StreamWriter writer = new StreamWriter("C:\\Users\\risha\\OneDrive\\Documents\\names.txt"); // if file name does not exist , it will be created by OS
+            writer.WriteLine(name);
+            writer.Close();
+        }
+    }
+}
